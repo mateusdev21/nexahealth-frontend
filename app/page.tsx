0abdf9d46b44236782b533713cc951c095abbd6f
mateusdev21 <mateus.dev21@gmail.com>
2025-07-18 16:46:16 +0700

    chore: add react icon

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
+import { FaBell } from "react-icons/fa";
+
 export default function Home() {
-  return <h1 className="text-3xl font-bold text-blue-600">Hello world!</h1>;
+  return (
+    <h1 className="text-3xl font-bold text-blue-600">
+      <FaBell className="inline mr-2" />
+      Hello world!
+    </h1>
+  );
 }
